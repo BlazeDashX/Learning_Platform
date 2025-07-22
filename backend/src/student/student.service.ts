f8ad5c029d15812d31d389da4b357a7be769d7e9
imNHK07 <nafis.7337@gmail.com>
2025-07-22 23:24:13 +0600

    Update student.service.ts

---
+++ backend/src/student/student.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class StudentService {
+    private students=[
+        {id:1, name:"Nafis", age:23},
+        {id:2, name:"Zakir", age:24},
+    ];
+
+    getAllStudents(){
+        return this.students;
+    }
+
+    getStudentById(id:number){
+        const student = this.students.find((s) => s.id ===id); 
+        if(!student) throw new NotFoundException('student not found');
+        return student;
+    }
+
+    //post
+
+    createStudent(data:{name:string; age:number})
+    {
+        const newStudent = {
+            id:Date.now(),
+            ...data,
+
+        };
+        this.students.push(newStudent);
+        return newStudent;
+    }
+
+    //put
+    updateStudent(id:number, data:{name:string; age:number}){
+        const index= this.students.findIndex((s)=> s.id===id);
+        if(index === -1) throw new NotFoundException('Not found');
+        this.students[index]={id, ...data};
+        return this.students;
+    }
+
+    //patch
+    patchStudent(id:number, data: Partial<{name:string; age:number}>){
+        const student = this.getStudentById(id);
+        Object.assign(student,data);
+        return student;
+    }
+
+    //delete
+    deleteStudent(id:number)
+    {
+        const index= this.students.findIndex((s)=> s.id===id);
+        if(index === -1) throw new NotFoundException('Not found');
+        const deleted=this.students.splice(index,1);
+        return {message: 'Student Deleted', student:deleted[0]};
+    }
+}
